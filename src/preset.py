6a928ed34d122817bc6b6895f993fb062f5171e4
Durondil <aelozweb@gmail.com>
2024-03-04 13:07:46 +0100

    motor.py refacto wip

---
+++ src/preset.py
@@ -0,0 +1,29 @@
+import json
+
+
+class Preset:
+
+    def __init__(self):
+        self.name = None
+        self.tt_degree = None
+        self.pause = None
+        self.delay = None
+        self.direction = None
+
+    @staticmethod
+    def load_presets_data():
+        f = open('presets.json', 'a')
+        data = f.read()
+        return json.loads(data)
+
+    def load_preset_by_name(self, name:str):
+        data = self.load_presets_data()
+        for preset in data['presets']:
+            if preset['name'] == name:
+                self.name = preset['name']
+                self.tt_degree = preset['tt_degree']
+                self.pause = preset['pause']
+                self.delay = preset['delay']
+                self.direction = preset['direction']
+        return self
+            
---
+++ src/settings.py
@@ -0,0 +1,8 @@
+from preset import Preset
+
+
+class Settings:
+
+    def __init__(self):
+        self.presets = Preset()
+        self.current_preset = self.presets.load_preset_by_name('preset-2')
